0441b564474c463e6db33be515eeb153d07089a1
Danel A. Newell <newell_daniel@yahoo.com>
2013-01-29 22:40:08 -0500

    This is a House

--- Project1.java
+++ Project1.java
@@ -10,8 +10,6 @@ import javax.media.opengl.GLCanvas;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
 
-
-
 /**
  * Project1.java <BR>
  * author: Brian Paul (converted to Java by Ron Cemer and Sven Goethel) <P>
@@ -20,21 +18,20 @@ import javax.media.opengl.glu.GLU;
  */
 public class Project1 implements GLEventListener {
 
+    private float rotateT = 0.0f;
+
     public static void main(String[] args) {
         Frame frame = new Frame("Simple JOGL Application");
         GLCanvas canvas = new GLCanvas();
 
         canvas.addGLEventListener(new Project1());
         frame.add(canvas);
-        frame.setSize(640, 480);
+        frame.setSize(600, 600);
         final Animator animator = new Animator(canvas);
         frame.addWindowListener(new WindowAdapter() {
 
             @Override
             public void windowClosing(WindowEvent e) {
-                // Run this on another thread than the AWT event queue to
-                // make sure the call to Animator.stop() completes before
-                // exiting
                 new Thread(new Runnable() {
 
                     public void run() {
@@ -44,23 +41,15 @@ public class Project1 implements GLEventListener {
                 }).start();
             }
         });
-        // Center frame
-        frame.setLocationRelativeTo(null);
+
         frame.setVisible(true);
         animator.start();
     }
 
     public void init(GLAutoDrawable drawable) {
-        // Use debug pipeline
-        // drawable.setGL(new DebugGL(drawable.getGL()));
-
         GL gl = drawable.getGL();
         System.err.println("INIT GL IS: " + gl.getClass().getName());
-
-        // Enable VSync
         gl.setSwapInterval(1);
-
-        // Setup the drawing area and shading mode
         gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
     }
@@ -68,9 +57,7 @@ public class Project1 implements GLEventListener {
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
         GL gl = drawable.getGL();
         GLU glu = new GLU();
-
-        if (height <= 0) { // avoid a divide by zero error!
-        
+        if (height <= 0) {
             height = 1;
         }
         final float h = (float) width / (float) height;
@@ -84,43 +71,109 @@ public class Project1 implements GLEventListener {
 
     public void display(GLAutoDrawable drawable) {
         GL gl = drawable.getGL();
-
-        // Clear the drawing area
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-        // Reset the current matrix to the "identity"
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+        gl.glClear(GL.GL_DEPTH_BUFFER_BIT);
         gl.glLoadIdentity();
+        gl.glTranslatef(0.0f, 0.0f, -5.0f);
 
-        // Move the "drawing cursor" around
-        gl.glTranslatef(-1.5f, 0.0f, -6.0f);
-
-        // Drawing Using Triangles
-        gl.glBegin(GL.GL_TRIANGLES);
-            gl.glColor3f(1.0f, 0.0f, 0.0f);    // Set the current drawing color to red
-            gl.glVertex3f(0.0f, 1.0f, 0.0f);   // Top
-            gl.glColor3f(0.0f, 1.0f, 0.0f);    // Set the current drawing color to green
-            gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
-            gl.glColor3f(0.0f, 0.0f, 1.0f);    // Set the current drawing color to blue
-            gl.glVertex3f(1.0f, -1.0f, 0.0f);  // Bottom Right
-        // Finished Drawing The Triangle
-        gl.glEnd();
-
-        // Move the "drawing cursor" to another position
-        gl.glTranslatef(3.0f, 0.0f, 0.0f);
-        // Draw A Quad
+        // Front Of House
         gl.glBegin(GL.GL_QUADS);
-            gl.glColor3f(0.5f, 0.5f, 1.0f);    // Set the current drawing color to light blue
-            gl.glVertex3f(-1.0f, 1.0f, 0.0f);  // Top Left
-            gl.glVertex3f(1.0f, 1.0f, 0.0f);   // Top Right
-            gl.glVertex3f(1.0f, -1.0f, 0.0f);  // Bottom Right
-            gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
-        // Done Drawing The Quad
+            gl.glColor3f(1.0f, 0.0f, 0.0f);
+            gl.glVertex3f(-1.0f, 1.0f, 0.0f);
+            gl.glVertex3f(1.0f, 1.0f, 0.0f);
+            gl.glVertex3f(1.0f, -1.0f, 0.0f);
+            gl.glVertex3f(-1.0f, -1.0f, 0.0f);
         gl.glEnd();
 
-        // Flush all drawing operations to the graphics card
-        gl.glFlush();
+        // Front Left Window
+        gl.glBegin(GL.GL_QUADS);
+            gl.glColor3f(0.5f, 0.0f, 0.0f);
+            gl.glVertex3f(-0.75f, 0.25f, 0.0f);
+            gl.glVertex3f(-0.25f, 0.25f, 0.0f);
+            gl.glVertex3f(-0.25f, 0.75f, 0.0f);
+            gl.glVertex3f(-0.75f, 0.75f, 0.0f);
+        gl.glEnd();
+
+        // Front Right Window
+        gl.glBegin(GL.GL_QUADS);
+            gl.glColor3f(0.5f, 0.0f, 0.0f);
+            gl.glVertex3f(0.75f, 0.25f, 0.0f);
+            gl.glVertex3f(0.25f, 0.25f, 0.0f);
+            gl.glVertex3f(0.25f, 0.75f, 0.0f);
+            gl.glVertex3f(0.75f, 0.75f, 0.0f);
+        gl.glEnd();
+
+        // Door
+        gl.glBegin(GL.GL_QUADS);
+            gl.glColor3f(0.0f, 1.0f, 0.0f);
+            gl.glVertex3f(-0.25f, -1.0f, 0.0f);
+            gl.glVertex3f(0.25f, -1.0f, 0.0f);
+            gl.glVertex3f(0.25f, -0.25f, 0.0f);
+            gl.glVertex3f(-0.25f, -0.25f, 0.0f);
+        gl.glEnd();
+
+        // Right Side of house
+        gl.glBegin(GL.GL_QUADS);
+            gl.glColor3f(1.0f, 0.0f, 0.0f);
+            gl.glVertex3f(1.0f, -1.0f, 0.0f);
+            gl.glVertex3f(2.0f, -1.0f, -1.0f);
+            gl.glVertex3f(2.0f, 1.0f, -1.0f);
+            gl.glVertex3f(1.0f, 1.0f, 0.0f);
+        gl.glEnd();
+
+        // Right Side of roof
+        gl.glBegin(GL.GL_QUADS);
+            gl.glColor3f(1.0f, 0.0f, 0.0f);
+            gl.glVertex3f(1.0f, 1.0f, 0.0f);
+            gl.glVertex3f(2.0f, 1.0f, -1.0f);
+            gl.glVertex3f(1.0f, 1.75f, -1.0f);
+            gl.glVertex3f(0.0f, 2.0f, -1.0f);
+        gl.glEnd();
+
+        // Roof
+        gl.glBegin(GL.GL_TRIANGLES);
+            gl.glColor3f(1.0f, 0.0f, 0.0f);
+            gl.glVertex3f(-1.0f, 1.0f, 0.0f);
+            gl.glVertex3f(0.0f, 2.0f, -1.0f);
+            gl.glVertex3f(1.0f, 1.0f, 0.0f);
+        gl.glEnd();
+
+        // Front Side Of House Outline
+        gl.glBegin(GL.GL_LINE_STRIP);
+            gl.glColor3f(0.0f, 0.0f, 1.0f);
+            gl.glVertex3f(-1.0f, 1.0f, 0.0f);
+            gl.glVertex3f(1.0f, 1.0f, 0.0f);
+            gl.glVertex3f(1.0f, -1.0f, 0.0f);
+            gl.glVertex3f(-1.0f, -1.0f, 0.0f);
+        gl.glEnd();
+
+        // Right Side of house outline
+        gl.glBegin(GL.GL_LINE_STRIP);
+            gl.glColor3f(0.0f, 0.0f, 1.0f);
+            gl.glVertex3f(1.0f, -1.0f, 0.0f);
+            gl.glVertex3f(2.0f, -1.0f, -1.0f);
+            gl.glVertex3f(2.0f, 1.0f, -1.0f);
+            gl.glVertex3f(1.0f, 1.0f, 0.0f);
+        gl.glEnd();
+
+        // Roof Outline
+        gl.glBegin(GL.GL_LINE_STRIP);
+            gl.glColor3f(0.0f, 0.0f, 1.0f);
+            gl.glVertex3f(-1.0f, 1.0f, 0.0f);
+            gl.glVertex3f(0.0f, 2.0f, -1.0f);
+            gl.glVertex3f(1.0f, 1.0f, 0.0f);
+        gl.glEnd();
+
+        // Right Side of roof Outline
+        gl.glBegin(GL.GL_LINE_STRIP);
+            gl.glColor3f(0.0f, 0.0f, 1.0f);
+            gl.glVertex3f(1.0f, 1.0f, 0.0f);
+            gl.glVertex3f(2.0f, 1.0f, -1.0f);
+            gl.glVertex3f(1.0f, 1.75f, -1.0f);
+            gl.glVertex3f(0.0f, 2.0f, -1.0f);
+        gl.glEnd();
     }
 
     public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
     }
 }
-
